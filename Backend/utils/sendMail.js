b463c59834a4e67d7474a21ea9b6e14f98b0ab7a
Madhari Tarunsai <saitarunsai04062002@gmail.com>
2026-05-14 13:07:50 +0530

    gridemail

--- Backend/utils/sendMail.js
+++ Backend/utils/sendMail.js
@@ -1,37 +1,21 @@
-const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
 require("dotenv").config();
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendMail = async (to, subject, html) => {
   try {
-    console.log("sendMail function started...");
-    console.log("Sending mail to:", to);
-    console.log("Sender Email:", process.env.EMAIL_USER);
-    console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
-
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    console.log("Transporter created successfully");
-
-    const info = await transporter.sendMail({
-      from: `"Event Management" <${process.env.EMAIL_USER}>`,
+    const msg = {
       to,
+      from:process.env.EMAIL_USER,
       subject,
       html,
-    });
+    };
 
+    await sgMail.send(msg);
     console.log("Mail sent successfully");
-    console.log("Mail response:", info.response);
-
-    return info;
   } catch (error) {
-    console.log("Email error full:", error);
-    console.log("Email error message:", error.message);
+    console.log("SendGrid error:", error.message);
   }
 };
 
